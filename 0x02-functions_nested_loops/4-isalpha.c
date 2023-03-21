d864c538bf0f3fd80c23fe9cd5d8dca4b591e413
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-03-21 16:33:43 -0700

    checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,30 +1,13 @@
 #include "main.h"
 
 /**
- * print_sign - Determines if the input number
- * greater, iqual or less than zero.
- *
- * @n: The input number as an integer.
- *
- * Return: 1 > 0. 0 == 0.
- * -1 < 0.
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
  */
-int print_sign(int n)
+
+int _isalpha(int c)
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
-	_putchar('\n');
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+
 }
