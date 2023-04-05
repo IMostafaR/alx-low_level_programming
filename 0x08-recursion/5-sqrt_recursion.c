17ab190c9780dc6a127d5fcf2c4d56bf2ee998ca
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-04-05 05:44:19 -0700

    fix issue

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,3 +16,19 @@ int _sqrt_check(int n, int i)
 	else
 		return (_sqrt_check(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: number to find square root of
+ * Return: natural square root of n or -1 if it doesn't have one
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else
+		return (_sqrt_check(n, 1));
+}
